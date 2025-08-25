a84cb8ef623bbcc5cf436f32e1efdd7904589b2e
abdo <abdelrahmanabuharag@gmail.com>
2025-08-25 14:50:57 +0300

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ import logo from '../../default-monochrome-white.svg'
 import logoBlack from '../../default-monochrome-black.svg'
 const Header = () => {
     const [mobileMenu, setMobileMenu] = useState(false);
-    const [theme , setTheme] = useState(
+    const [theme, setTheme] = useState(
         localStorage.getItem("theme") ?? "dark"
     );
     useEffect(() => {
@@ -25,81 +25,86 @@ const Header = () => {
     }, [theme]);
     console.log(logo)
     return (
-        <header className='container' data-aos="fade-up">
-            
-            <div className="logo">
-                <Link to='/'>
-                    <img src= {logo}
-                        alt='Logo' 
-                        width={150}
-                        height={70}
+        <>
+            <div className={`overlay ${mobileMenu ? 'active' : ''}`} onClick={() => {
+                setMobileMenu(false)
+            }}></div>
+            <header className='container' data-aos="fade-up">
+                <div className="logo">
+                    <Link to='/'>
+                        <img src={logo}
+                            alt='Logo'
+                            width={150}
+                            height={70}
                         />
-                </Link>
-                
-            </div>
-            <nav>
-                <ul>
-                    {
-                        navItems.map((item) => (
-                            <li key={item.name}>
-                                <a href={item.link}>
-                                    {item.icon}
-                                    <span>
-                                    {item.name}
-                                    </span>
-                                </a>
-                            </li>
-                        ))
-                    }
-                </ul>
-            </nav>
-            <div className='theme'>
-            <button
-                onClick={() => {
-                    localStorage.setItem(
-                        "theme",
-                        theme === "dark" ? "light" : "dark"
-                    );
-                    setTheme(localStorage.getItem("theme"));
-                }}
-            >
-                {theme === "dark" ? (
-                    <Moon size={20} color="#374fff" strokeWidth={3} />
-                ) : (
-                    <Sun size={20} color="#FBD94F" strokeWidth={3} />
-                )}
-            </button>
-            <button className="show" onClick={() => {
-                setMobileMenu(true);
-            }}>
-                <Menu />
-            </button>
-            </div>
-            {
-                mobileMenu &&
-                <div className="fixed">
-                    <ul>
-                    <div className="close" onClick={() => {
-                            setMobileMenu(false)
-                        }}>
-                            <SquareX size={36} strokeWidth={2.25} />
-                        </div>
-                    {
-                        navItems.map((item) => (
-                            <li key={item.name}>
-                                <a href={item.link}>
-                                    {item.icon}
-                                    <span>
-                                    {item.name}
-                                    </span>
-                                </a>
-                            </li>
-                        ))
-                    }
-                </ul>
+                    </Link>
                 </div>
-            }
-        </header>
+                <nav>
+                    <ul>
+                        {
+                            navItems.map((item) => (
+                                <li key={item.name}>
+                                    <Link to={item.link}>
+                                        {item.icon}
+                                        <span>
+                                            {item.name}
+                                        </span>
+                                    </Link>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </nav>
+                <div className='theme'>
+                    <button
+                        onClick={() => {
+                            localStorage.setItem(
+                                "theme",
+                                theme === "dark" ? "light" : "dark"
+                            );
+                            setTheme(localStorage.getItem("theme"));
+                        }}
+                    >
+                        {theme === "dark" ? (
+                            <Moon size={20} color="#374fff" strokeWidth={3} />
+                        ) : (
+                            <Sun size={20} color="#FBD94F" strokeWidth={3} />
+                        )}
+                    </button>
+                    <button className="show" onClick={() => {
+                        setMobileMenu(true);
+                    }}>
+                        <Menu />
+                    </button>
+                </div>
+                {
+                    mobileMenu &&
+                    <>
+                        <div className="fixed">
+                            <ul>
+                                <div className="close" onClick={() => {
+                                    setMobileMenu(false)
+                                }}>
+                                    <SquareX size={36} strokeWidth={2.25} />
+                                </div>
+                                {
+                                    navItems.map((item) => (
+                                        <li key={item.name}>
+                                            <a href={item.link}>
+                                                {item.icon}
+                                                <span>
+                                                    {item.name}
+                                                </span>
+                                            </a>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                    </>
+                }
+            </header>
+        </>
     )
 }
 
--- src/components/Hero/HeroSectionOne.jsx
+++ src/components/Hero/HeroSectionOne.jsx
@@ -4,52 +4,52 @@ import gradient from './gradient.png'
 import Spline from '@splinetool/react-spline'
 const HeroSectionOne = () => {
     return (
-    <section>
-        <img className='image-gradient'
-        src={gradient} alt="gradient" width={100} height={100} />
-        <div className="layer-blure"></div>
-        <div className='container'>
-            <main data-aos="fade-up">
-                <div className="content">
-                    <div className="tag-box" data-aos="zoom-in" data-aos-delay="200">
-                        <div className="tag">
-                            INTRODUCING 
+        <section>
+            <img className='image-gradient'
+                src={gradient} alt="gradient" width={100} height={100} />
+            <div className="layer-blure"></div>
+            <div className='container'>
+                <main data-aos="fade-up">
+                    <div className="content">
+                        <div className="tag-box" data-aos="zoom-in" data-aos-delay="200">
+                            <div className="tag">
+                                INTRODUCING
+                            </div>
                         </div>
-                    </div>
                         <h1>YOUR VISION<br />PERFECTED</h1>
                         <p className='description'>
-                        At EMRELED,We provide Web design, social media, photography, printing, and flight booking.
+                            At EMRELED,We provide Web design, social media, photography, printing, and flight booking.
                         </p>
                         <div className="buttons" data-aos="fade-up" data-aos-delay="400">
-                        <a href='/contact' 
-                        className="cssbuttons-io-button">
-                            Get started
-                            <div className="icon">
-                                <svg
-                                height="24"
-                                width="24"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                                >
-                                <path d="M0 0h24v24H0z" fill="none"></path>
-                                <path
-                                    d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
-                                    fill="currentColor"
-                                ></path>
-                                </svg>
-                            </div>
-                        </a>
-                        <a href='/about' className="button">
-                        See All
-                        <span className="button-span"> ─ Now</span>
-                        </a>
+                            <a href='/contact'
+                                className="cssbuttons-io-button">
+                                Get started
+                                <div className="icon">
+                                    <svg
+                                        height="24"
+                                        width="24"
+                                        viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path d="M0 0h24v24H0z" fill="none"></path>
+                                        <path
+                                            d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                                            fill="currentColor"
+                                        ></path>
+                                    </svg>
+                                </div>
+                            </a>
+                            <a href='/about' className="button-hero">
+                                See All
+                                <span className="button-span"> ─ Now</span>
+                            </a>
                         </div>
-                </div>
-            </main>
-            <Spline className='robot-3d' scene="https://prod.spline.design/JA4ZMUOUkTYTMgJ2/scene.splinecode" />
-            
-        </div>
-    </section>
+                    </div>
+                </main>
+                <Spline className='robot-3d' scene="https://prod.spline.design/JA4ZMUOUkTYTMgJ2/scene.splinecode" />
+
+            </div>
+        </section>
     )
 }
 
